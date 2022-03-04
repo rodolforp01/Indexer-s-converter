2c5f21287d91a2445187c64505a7780f12aeaf79
Indexer <Indexer@DESKTOP-QGUJMOQ>
2022-03-04 13:02:29 -0800

    Add project files.

---
+++ Indexer's converter/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Indexer_s_converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ConverterOutp = new System.Windows.Forms.TextBox();
+            this.btn = new System.Windows.Forms.Button();
+            this.NormalString = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // ConverterOutp
+            // 
+            this.ConverterOutp.Location = new System.Drawing.Point(12, 209);
+            this.ConverterOutp.Multiline = true;
+            this.ConverterOutp.Name = "ConverterOutp";
+            this.ConverterOutp.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.ConverterOutp.Size = new System.Drawing.Size(483, 116);
+            this.ConverterOutp.TabIndex = 0;
+            // 
+            // btn
+            // 
+            this.btn.Location = new System.Drawing.Point(206, 120);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(92, 55);
+            this.btn.TabIndex = 3;
+            this.btn.Text = "Convert";
+            this.btn.UseVisualStyleBackColor = true;
+            this.btn.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // NormalString
+            // 
+            this.NormalString.Location = new System.Drawing.Point(75, 74);
+            this.NormalString.Name = "NormalString";
+            this.NormalString.Size = new System.Drawing.Size(352, 22);
+            this.NormalString.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(507, 353);
+            this.Controls.Add(this.NormalString);
+            this.Controls.Add(this.btn);
+            this.Controls.Add(this.ConverterOutp);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox ConverterOutp;
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.TextBox NormalString;
+    }
+}
+
---
+++ Indexer's converter/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Indexer_s_converter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            ConverterOutp.Clear();
+            string value1 = Encrypt2(NormalString.Text.ToString());
+            string value2 = toHex(value1);
+            string value3 = Organize(value2);
+            ConverterOutp.Text = value3.ToString();
+        }
+
+        private static Random random = new Random();
+        public static string Encrypt2(string a)
+        {
+            List<string> list = new List<string>();
+            string text = "";
+            foreach (char value in a)
+            {
+                list.Add(Convert.ToInt32(value).ToString());
+            }
+            foreach (string value2 in list)
+            {
+                int num = Convert.ToInt32(value2);
+                for (int j = 0; j < 2; j++)
+                {
+                    int num2 = random.Next(0, num);
+                    num -= num2;
+                    if (num2.ToString().Length == 1)
+                    {
+                        text += (35u + Convert.ToUInt32("0" + num2.ToString())).ToString("X");
+                    }
+                    else
+                    {
+                        text += (num2 + 35).ToString("X");
+                    }
+                    if (j == 1)
+                    {
+                        if (num.ToString().Length == 1)
+                        {
+                            text += (35u + Convert.ToUInt32("0" + num.ToString())).ToString("X");
+                        }
+                        else
+                        {
+                            text += (num + 35).ToString("X");
+                        }
+                    }
+                }
+            }
+            return text;
+        }
+
+        //converts it to hex
+        public string toHex(string inp)
+        {
+            string outp = string.Empty;
+            char[] value = inp.ToCharArray();
+            foreach(char L in value)
+            {
+                int V = Convert.ToInt32(L);
+                outp += String.Format("{0:x}", V);
+            }
+
+            return outp;
+        }
+
+        static string Organize(string value)
+        {
+            string value2 = Regex.Replace(value, @"[0-9A-F]{2}", "0x$0, ");
+            return value2;
+        }
+
+
+    }
+}
